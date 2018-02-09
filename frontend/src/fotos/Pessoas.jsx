0f3966720881741b4119d349a34d27d61841a8ff
Victor Dolirio <victor.barbosa@tst.jus.br>
2018-02-09 17:59:13 -0200

    Adicionando um exemplo de como fazer um upload via javascript

--- frontend/src/fotos/Pessoas.jsx
+++ frontend/src/fotos/Pessoas.jsx
@@ -45,6 +45,45 @@ export class Pessoas extends React.Component {
         });
     }
 
+    /****************************************************************
+     * Teste de upload
+     * // Select your input type file and store it in a variable
+const input = document.getElementById('fileinput');
+
+// This will upload the file after having read it
+const upload = (e) => {
+  fetch('www.example.net', { // Your POST endpoint
+    method: 'POST',
+    headers: {
+      "Content-Type": "You will perhaps need to define a content-type here"
+    },
+    body: e.currentTarget.result // This is the content of your file
+  }).then(
+    response => response.json() // if the response is a JSON object
+  ).then(
+    success => console.log(success); // Handle the success response object
+  ).catch(
+    error => console.log(error); // Handle the error response object
+  );
+};
+
+// Event handler executed when a file is selected
+const onSelectFile = (files) => {
+  // Files is a list because you can select several files
+  // We just upload the first selected file
+  const file = input.files[0];
+  const reader = new FileReader();
+
+  // We read the file and call the upload function with the result
+  reader.onload = upload;
+  reader.readAsText(file);
+};
+
+// Add a listener on your input
+// It will be triggered when a file will be selected
+input.addEventListener('change', onSelectFile , false);
+     */
+
     render() {
         return (
             <div className="container">
